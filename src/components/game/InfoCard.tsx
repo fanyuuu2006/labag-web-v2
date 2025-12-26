8672bab79b5ad3a2adc5115840cd10253f3648fd
Yorn90104 <bingxiao526@gmail.com>
2025-12-27 00:04:49 +0800

    feat: 調整 InfoCard 組件的內邊距，優化排版

--- src/components/game/InfoCard.tsx
+++ src/components/game/InfoCard.tsx
@@ -79,7 +79,7 @@ export const InfoCard = memo(({ className, ...rest }: InfoCardProps) => {
   return (
     <div
       className={cn(
-        "bg-black/40 rounded-md p-3 md:p-6 flex flex-col gap-2 sm:gap-3 items-start sm:items-center shadow-md",
+        "bg-black/40 rounded-md p-4 flex flex-col gap-2 sm:gap-3 items-start sm:items-center shadow-md",
         className
       )}
       {...rest}
