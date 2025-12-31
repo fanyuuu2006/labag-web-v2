9036440c1a6ae881a057382e226fd6e6d7ed7333
Yorn90104 <bingxiao526@gmail.com>
2025-12-31 15:45:45 +0800

    feat: 更新 MainSection 組件以顯示用戶資料缺失提示，修正容器類名錯誤；移除 fetcher 函數中的錯誤處理邏輯

--- src/components/profile/[id]/MainSection.tsx
+++ src/components/profile/[id]/MainSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { GlowText } from "@/components/GlowText";
 import { SupabaseAllowFieldsUser, SupabaseRecord } from "@/types/backend";
 
 export const MainSection = ({
@@ -11,7 +12,17 @@ export const MainSection = ({
 }) => {
   return (
     <section className="h-full">
-      <div className="contianer h-full">{}</div>
+      <div className="container h-full">
+        {user ? (
+          <></>
+        ) : (
+          <div className="flex flex-col items-center justify-center h-full">
+            <div className="card p-4">
+              <GlowText as="h2">未找到該用戶資料</GlowText>
+            </div>
+          </div>
+        )}
+      </div>
     </section>
   );
 };
--- src/utils/fetcher.tsx
+++ src/utils/fetcher.tsx
@@ -2,8 +2,5 @@ export async function fetcher<T>(
   ...args: Parameters<typeof fetch>
 ): Promise<T> {
   const res = await fetch(...args);
-  if (!res.ok) {
-    throw new Error("Fetch error");
-  }
   return res.json();
 }
