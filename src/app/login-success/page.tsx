400031f0310322325b44b8b232a19dc4e8abc87f
Yorn90104 <bingxiao526@gmail.com>
2026-01-02 01:02:59 +0800

    feat: 整合 useUser 以獲取用戶資訊並重定向至個人頁面

--- src/app/login-success/page.tsx
+++ src/app/login-success/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { GlowText } from "@/components/GlowText";
-import { LOCAL_STORAGE_KEY } from "@/contexts/UserContext";
+import { LOCAL_STORAGE_KEY, useUser } from "@/contexts/UserContext";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function LoginSeccess() {
   const router = useRouter();
+  const {user} = useUser();
 
   useEffect(() => {
     // 從 URL 取得 token
@@ -14,12 +15,12 @@ export default function LoginSeccess() {
     if (token) {
       // 儲存 token 到 localStorage
       localStorage.setItem(LOCAL_STORAGE_KEY, token);
-      router.replace("/profile");
+      router.replace(`/profile/${user?.id}`); // 重定向到個人頁面
     } else {
       console.error("未成功取得 Token");
       router.replace("/"); // 跳回登入頁面
     }
-  }, [router]);
+  }, [router, user?.id]);
 
   return (
     <section className="h-full">
