4b558d5bfaafb8bce4bd25676ddf25cf6a9deb5b
Yorn90104 <bingxiao526@gmail.com>
2026-01-06 01:03:21 +0800

    feat: 增加 fetcher 函數的錯誤處理，當請求失敗時記錄錯誤訊息

--- src/utils/fetcher.tsx
+++ src/utils/fetcher.tsx
@@ -2,5 +2,8 @@ export async function fetcher<T>(
   ...args: Parameters<typeof fetch>
 ): Promise<T> {
   const res = await fetch(...args);
+  if (!res.ok) {
+    console.error("Fetch 錯誤：", res.statusText);
+  }
   return res.json();
 }
