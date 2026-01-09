977a08073858558ed778cdfba129aa29d0f35d1e
Yorn90104 <bingxiao526@gmail.com>
2026-01-09 21:28:33 +0800

    feat: 更新皮卡丘模式的可玩次數計算邏輯，增加最大值判斷

--- src/libs/game.tsx
+++ src/libs/game.tsx
@@ -186,7 +186,7 @@ export const description: Record<
               <>
                 模式期間處現任一`
                 {game.getMode("pikachu")?.variable.bindPattern.name}`
-                ，則額外增加 <K>+(當前已觸發皮卡丘充電次數)</K> 次可玩次數。
+                ，則額外增加 <K>+(當前已觸發皮卡丘充電次數 與 {game.getMode("pikachu")?.variable.bindPattern.bonusRounds}|取最大值)</K> 次可玩次數。
               </>
             ),
           },
